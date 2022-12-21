471c9770a61547ee19093f1b8d3978020ae9266c
Deiny Plata <deinyplata.04@gmail.com>
2022-12-21 12:47:17 -0600

    modificacion del main.js, en donde se renombran las variables para almacenar el valor porcentual, creacion de constantes para almacenar el text de invalid y crear funcion para actualizar el DOM

--- JS/main.js
+++ JS/main.js
@@ -22,8 +22,18 @@ const personas = document.getElementById("total-people");
 const btns = document.querySelectorAll(".form__box__container__btn");
 const resetBtn = document.getElementById("reset-btn");
 
+// Elemento para almacebar texto del invalid (para validar)
+const invalid1 = document.getElementById("not-valid1");
+const invalid2 = document.getElementById("not-valid2");
+const invalid3 = document.getElementById("not-valid3");
+
+// Variable para almacenar el valor porcentual seleccionado
+let porcenTip;
+let checkCustom;
+let checkBtn = false;
+
 // Evento para escuchar a que boton le estamos dando click
-porcenDiv.addEventListener("click", (e) => {
+porcenDiv.addEventListener('click', (e) => {
   for (let i = 0; i < btns.length; i++) {
     btns[i].classList.remove("active");
   }
@@ -32,36 +42,87 @@ porcenDiv.addEventListener("click", (e) => {
     checkBtn = true;
   }
 
+  porcenTip = e.target;
+  porcenTip.classList.add('active');
+
   // Condicion para escuchar el custom
   if (e.target === customTip) {
     porcenTip = e.target;
+
+    checkCustom = true;
+
+    porcenTip.classList.remove('active');
   }
 });
 
 // Crear un evento de submit para la forma
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   // Enviar la action por defecto
   e.preventDefault();
 
-  // Crear objeto constante con los valores de la forma
-  const formulario = {
-    subTotalF: subtotal.value,
-    porcenTipF: porcenTip.value,
-    personasF: personas.value,
-  };
+  if (validateClick(checkBtn)) {
+    //Si le dieron click a custom, entonces dividamos su valor entre 10
+    if (checkCustom) {
+      porcenTip.value = porcenTip.value / 100;
+    }
+
+    // Crear objeto constante con los valores de la forma
+    const formulario = {
+      subTotalF: subtotal.value,
+      porcenTipF: porcenTip.value,
+      personasF: personas.value,
+    }
+
+    const { subTotalF, porcenTipF, personasF } = formulario;
+
+    if (validateAll(subTotalF, personasF)) {
+      updateDOM(subTotalF, porcenTipF, personasF);
+      subtotal.parentElement.classList.add("valid");
+      personas.parentElement.classList.add("valid");
+
+      removeBorder(subtotal, "valid");
+      removeBorder(personas, "valid");
+    }
+  }
 });
 
 // Funcion para actualizar el DOM
 
-//Llamar  a las funciones que actualizan el DOM
-updateTip(tipFinal.toFixed(2), tipResult);
-updateTotal(totalF.toFixed(2), totalResult);
+function updateDOM(subTotalF, porcenTipF, personasF) {
+  const tipFinal = tipCalculater(subTotalF, porcenTipF, personasF);
+  
+  const totalF = totalFinal(subTotalF, personasF, tipFinal);
+
+  //Llamar  a las funciones que actualizan el DOM
+  updateTip(tipFinal.toFixed(2), tipResult);
+  updateTotal(totalF.toFixed(2), totalResult);
+}
+
+function removeText(element) {
+  setTimeout(() => {
+    element.innerText = "";
+  }, 4000);
+}
+
+function removeBorder(element, classOfElement) {
+  setTimeout(() => {
+    element.parentElement.classList.remove(classOfElement);
+  }, 4000);
+}
 
 // Boton para reiniciar la Tip calculator
 resetBtn.addEventListener("click", (e) => {
   subtotal.value = "";
   customTip.value = "";
   personas.value = "";
+
+  // Quitar validos
+  subtotal.parentElement.classList.remove("valid");
+  personas.parentElement.classList.remove("valid");
+
+  // Quitar invalidos
+  personas.parentElement.classList.remove("invalid");
+  subtotal.parentElement.classList.remove("invalid");
 
   for (let i = 0; i < btns.length; i++) {
     btns[i].classList.remove("active");
